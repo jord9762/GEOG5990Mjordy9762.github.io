0d6b1f4c3dc65c515205e6b7fc1a59421adad6ed
jord9762 <68658177+jord9762@users.noreply.github.com>
2021-01-27 15:45:18 +0000

    Add files via upload

---
+++ ex9.py
@@ -0,0 +1,16 @@
+# Here's some strange stuff, remember type it exactly
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+#the back slash with n after causes the moths to e printed on new lines in a list
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+print("""
+There's something going on here.
+With the three double quotes.
+We'll be able to type as much as we like. 
+Even 4 lines if we want, or 5, or 6.
+""")
+print("""Whats's
+ going on""")
