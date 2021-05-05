743af85bbc9be356a060106b62a888f84ee108f1
Jordan Reynoldson <68658177+jord9762@users.noreply.github.com>
2021-05-05 19:24:47 +0100

    Update code_shrinking_II.py

--- Programming_for_GIA_Core_Skills/Assessment_1/Task2_code_shrinking/code_shrinking_II.py
+++ Programming_for_GIA_Core_Skills/Assessment_1/Task2_code_shrinking/code_shrinking_II.py
@@ -1,5 +1,8 @@
+#importing the random package, able to create psuedo random number for testing purposes
 import matplotlib.pyplot
+#allows additional operations such as the itemgetter function
 import operator
+#allows the presentation of plots,graphs etc.. will be used to display agents
 import random
 
 
@@ -13,13 +16,12 @@ num_of_iterations = 100
 up to 10 times from 0-9 as this is the number of agents specified"""
 for i in range(num_of_agents):
     agents.append([random.randint(0,100),random.randint(0,100)])
-    print (agents)
+    #Below is a testing measure to make sure agents have been generated logically and we have the anticipated number. 
+    #print (agents)
 """Running all of the above code in one will show the process of this"""
 
     
-  """The code below means if random boolean is less than 1 add 1 and if greater than 0.5
-subtract 1, this will be done for the number of iterations specified which in this case is 
-100. Using the modulous symbol (return the remainder) means the value will be set to 0 if the calculation is greater than 99. This will prevent the Index out range error. """  
+  #The code below means if random boolean is less than 1 add 1 and if greater than 0.5 subtract 1  
 for j in range (num_of_iterations):
             for i in range(num_of_agents):
                   if random.random() < 0.5:
@@ -33,22 +35,23 @@ for j in range (num_of_iterations):
                    agents[i][1] = (agents[i][1] - 1) % 100
 
 
-
+#finds the max agents based upon the first integer of the co-ordinates
 print(max(agents))
-#finds the max agents based upon the first integer of the co-ordinates
 
+#finds the max agents based upon the second integer of the co-ordinates
 print(max(agents, key=operator.itemgetter(1)))
-#finds the max agents based upon the first integer of the co-ordinates
+
 
 
 for i in range(num_of_agents):
     matplotlib.pyplot.scatter(agents[i][1], agents[i][0])
     matplotlib.pyplot.scatter(agents[i][1], agents[i][0])
 
-matplotlib.pyplot.show()
-"""plots and shows the co-ordinates of all generated agents in position 0 (so the first value in our pair of
+ """plots and shows the co-ordinates of all generated agents in position 0 (so the first value in our pair of
 co-ordinates) and position 1 (Our second value in our pair of co-ordinates), plotting using a for loop ensures
-    that all co-ordinates are displayed on the map.""" 
+    that all co-ordinates are displayed on the map."""    
+matplotlib.pyplot.show()
+
 
 
 
